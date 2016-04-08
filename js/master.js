db0b32f36d28524d63f0a856ffe8662037f2ba73
Irio Musskopf <iirineu@gmail.com>
2016-04-08 10:24:20 -0300

    Define .section height based on window visible area

--- js/master.js
+++ js/master.js
@@ -1,29 +1,31 @@
 $(document).ready(function() {
-  var headerHeight = $(".section-header").outerHeight() - 50;
+  var sectionMinHeight = $(window.top).outerHeight();
+  $(".section").css("min-height", sectionMinHeight);
+  var headerHeight = $(".section-header").height();
 
   var divs = $(".section").map(function(_index, section) {
-    var wrappedSection = $(section)
+    var wrappedSection = $(section);
+    var bottom = wrappedSection.offset().top + wrappedSection.outerHeight() - 33;
+    // var bottom = wrappedSection.offset().top + wrappedSection.outerHeight() - 62;
     return {
-      bottom: wrappedSection.offset().top + wrappedSection.outerHeight(),
+      bottom: bottom,
       section: wrappedSection,
      };
   });
-
-  var height = $(".section").outerHeight();
 
   $(window).scroll(function() {
     var scrolledPast = divs.filter(function(_index, elem) {
       return elem.bottom < $(window).scrollTop() + headerHeight;
     });
-    var selected = $(divs[scrolledPast.length].section)
-    var name = selected.attr("id")
-    var selectedAnchor = $(".section-anchor[href='#" + name + "']")
-    $(".section-anchor").not(selectedAnchor).removeClass("active")
-    $(".section").not(selected).removeClass("with-fixed-header")
+    var selected = $(divs[scrolledPast.length].section);
+    var name = selected.attr("id");
+    var selectedAnchor = $(".section-anchor[href='#" + name + "']");
+    $(".section-anchor").not(selectedAnchor).removeClass("active");
+    $(".section").not(selected).removeClass("with-fixed-header");
 
-    $("body").attr("data-active", name)
-    selectedAnchor.addClass("active")
-    selected.addClass("with-fixed-header")
+    $("body").attr("data-active", name);
+    selectedAnchor.addClass("active");
+    selected.addClass("with-fixed-header");
   });
 
 });
