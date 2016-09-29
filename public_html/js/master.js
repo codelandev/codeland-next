35e16c80aef2124b9da980ec2487954c989ce3fd
Irio Musskopf <iirineu@gmail.com>
2016-09-28 23:35:50 -0300

    Responsive Slogan and contact

--- public_html/js/master.js
+++ public_html/js/master.js
@@ -1,44 +1,55 @@
+var didScroll = false,
+  divs = [],
+  headerHeight = 0;
+
 var calculateHeaderChanges = function () {
-  var showSelectedHeader = function () {
-    var scrolledPast = divs.filter(function(_index, elem) {
-      return elem.bottom < $(window).scrollTop() + headerHeight;
-    });
-    var selected = $(divs[scrolledPast.length].section);
-    var name = selected.attr("id");
-    var selectedAnchor = $(".section-anchor[href='#" + name + "']");
-    $(".section-anchor").not(selectedAnchor).removeClass("active");
-    $(".section").not(selected).removeClass("with-fixed-header");
-
-    $("body").attr("data-active", name);
-    selectedAnchor.addClass("active");
-    selected.addClass("with-fixed-header");
-  }
-
   var sectionMinHeight = $(window.top).outerHeight();
   $(".section").css("min-height", sectionMinHeight);
-  var headerHeight = $(".section-header").height();
+  headerHeight = $(".section-header:last").height();
 
-  var divs = $(".section").map(function(_index, section) {
+  divs = $(".section").map(function (_index, section) {
     var wrappedSection = $(section);
     var bottom = wrappedSection.offset().top +
-      wrappedSection.outerHeight() - 95;
+      wrappedSection.outerHeight();
     return {
       bottom: bottom,
       section: wrappedSection,
     };
   });
 
-  showSelectedHeader();
-  $(window).scroll(function() {
-    showSelectedHeader();
-  });
+  showSelectedHeader(divs, headerHeight);
 }
 
-$(document).ready(function() {
+var showSelectedHeader = function (divs, headerHeight) {
+  var scrolledPast = divs.filter(function (_index, elem) {
+    return elem.bottom < $(window).scrollTop() + headerHeight;
+  });
+  var selected = $(divs[scrolledPast.length].section);
+  var name = selected.attr("id");
+  var selectedAnchor = $(".section-anchor[href='#" + name + "']");
+  $(".section-anchor").not(selectedAnchor).removeClass("active");
+  $(".section").not(selected).removeClass("with-fixed-header");
 
+  $("body").attr("data-active", name);
+  selectedAnchor.addClass("active");
+  selected.addClass("with-fixed-header");
+}
+
+$(document).ready(function () {
   calculateHeaderChanges();
-  window.onresize = function() {
+  window.onresize = function () {
     calculateHeaderChanges();
   };
+
+  $(window).scroll(function () {
+    didScroll = true;
+  });
+
+  setInterval(function () {
+    if (didScroll) {
+      didScroll = false;
+      showSelectedHeader(divs, headerHeight);
+    }
+  }, 250);
 
 });
