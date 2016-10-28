5cb864738e3066dc692b68aa3650075e79de8972
Filipe Linhares <filipelinhares@outlook.com>
2016-10-28 19:09:32 -0200

    Improves animations

--- public_html/js/master.js
+++ public_html/js/master.js
@@ -1,57 +1,23 @@
-var didScroll = false,
-  divs = [],
-  headerHeight = 0;
-
-var calculateHeaderChanges = function () {
-  var sectionMinHeight = $(window.top).outerHeight();
-  $(".section").css("min-height", sectionMinHeight);
-  headerHeight = $(".section-header:last").height();
-
-  divs = $(".section").map(function (_index, section) {
-    var wrappedSection = $(section);
-    var bottom = wrappedSection.offset().top +
-      wrappedSection.outerHeight();
-    return {
-      bottom: bottom,
-      section: wrappedSection,
-    };
-  });
-
-  showSelectedHeader(divs, headerHeight);
-}
-
-var showSelectedHeader = function (divs, headerHeight) {
-  var scrolledPast = divs.filter(function (_index, elem) {
-    return elem.bottom < $(window).scrollTop() + headerHeight;
-  });
-  var selected = $(divs[scrolledPast.length].section);
-  var name = selected.attr("id");
-  var selectedAnchor = $(".section-anchor[href='#" + name + "']");
-  $(".section-anchor").not(selectedAnchor).removeClass("active");
-  $(".section").not(selected).removeClass("with-fixed-header");
-
-  $("body").attr("data-active", name);
-  selectedAnchor.addClass("active");
-  selected.addClass("with-fixed-header");
-}
+// TODO: Use https://github.com/camwiegert/in-view instead the code below
 
 $(document).ready(function () {
-  calculateHeaderChanges();
-  window.onresize = function () {
-    calculateHeaderChanges();
-  };
+  inView('[data-real-section]')
+    .on('enter', function (el) {
+      document.querySelector('#' + el.id + ' .section-header').classList.add('active')
+      document.body.dataset.active = el.id
+    })
+    .on('exit', function (el) {
+      document.querySelector('#' + el.id + ' .section-header').classList.remove('active')
+    })
+  ;
 
-  $(window).scroll(function () {
-    didScroll = true;
+  var vh = window.innerHeight;
+  inView.offset(vh / 2);
+
+  window.addEventListener('resize', () => {
+    vh = window.innerHeight;
+    inView.offset(vh / 2);
   });
-
-  setInterval(function () {
-    if (didScroll) {
-      didScroll = false;
-      showSelectedHeader(divs, headerHeight);
-    }
-  }, 500);
-
 
   particlesJS('home', {
     "particles": {
